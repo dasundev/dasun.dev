ef8f66c65a516f7e12c0062c5d07236bc4e4ab64
Dasun Tharanga <hello@dasun.dev>
2024-02-20 02:47:53 +0530

    fix: 404 error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.error')
 
-@section('title', '403')
+@section('title', '404')
 
 @section('content')
     <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
             <div class="mx-auto max-w-screen-sm text-center">
-                <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">403</h1>
-                <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">{{ __('Forbidden') }}</p>
-                <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">{{ __('You are not authorized to access this page.') }}</p>
+                <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
+                <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">{{ __("Something's missing.") }}</p>
+                <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">{{ __("Sorry, we can't find that page. You'll find lots to explore on the home page.") }}</p>
+                <a href="/" class="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">{{ __("Back to Homepage") }}</a>
             </div>
         </div>
     </section>
