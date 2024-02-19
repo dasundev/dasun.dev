a6aa73a542db8850c64700db2572b30d601291ae
Dasun Tharanga <hello@dasun.dev>
2024-02-19 23:53:01 +0530

    feat: Add custom error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.error')
+
+@section('title', '403')
+
+@section('content')
+    <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <div class="mx-auto max-w-screen-sm text-center">
+                <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">403</h1>
+                <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">{{ __('Forbidden') }}</p>
+                <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">{{ __('You are not authorized to access this page.') }}</p>
+            </div>
+        </div>
+    </section>
+@endsection
