aa58caf5b731e0be33c69c82c8ffd74edb6eca20
Dasun Tharanga <hello@dasun.dev>
2024-02-21 22:55:22 +0530

    fix: Display private posts in rss feeds

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -66,6 +66,6 @@ class Post extends Model implements Feedable
 
     public function getAllFeedItems(): Collection
     {
-        return Post::all();
+        return Post::visibility('public')->get();
     }
 }
