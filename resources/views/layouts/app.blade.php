9ba557eb4c764c732b961aa2ffb524c6898d365d
Dasun Tharanga <hello@dasun.dev>
2024-02-24 15:10:53 +0530

    fix: Title display (#14)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title'){{ $title ?? config('app.name', 'dasun.dev') }}</title>
+    <title>@yield('title', $title ?? config('app.name', 'dasun.dev'))</title>
 
     <!-- Favicon -->
     <link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
