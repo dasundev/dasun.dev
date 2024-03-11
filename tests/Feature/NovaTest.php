b32c681d44b01e3961093cf4525b53b9c9dd23de
Dasun Tharanga <hello@dasun.dev>
2024-03-11 20:38:18 +0530

    style: Pint fixes

--- tests/Feature/NovaTest.php
+++ tests/Feature/NovaTest.php
@@ -1,7 +1,7 @@
 <?php
 
-it("can render the nova login page", function () {
-    $response = $this->get("/admin/login");
+it('can render the nova login page', function () {
+    $response = $this->get('/admin/login');
 
     $response->assertStatus(200);
 });
