0ed818c8a6cd0f2efda1aed9d9f62f0a8d79ecfa
Dasun Tharanga <hello@dasun.dev>
2024-03-12 21:28:57 +0530

    style: Pint fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
