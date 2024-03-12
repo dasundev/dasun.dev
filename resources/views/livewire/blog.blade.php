ea1443aec009f5b987ac4d3b90e85874ebef3486
Dasun Tharanga <hello@dasun.dev>
2024-03-12 17:46:33 +0530

    feat: Add post meta data component

--- resources/views/livewire/blog.blade.php
+++ resources/views/livewire/blog.blade.php
@@ -7,17 +7,9 @@
         <article class="py-10 border border-gray-100 dark:border-gray-900 px-10 mb-8">
             <a class="text-xl lg:text-2xl font-normal text-black dark:text-white"
                 href="{{ route('show-post', $post->slug) }}" wire:navigate.hover>{{ $post->title }}</a>
-            <p class="text-sm text-gray-700 dark:text-gray-200 mt-2">
-                Published on
-                <time datetime="{{ $post->published_at }}">
-                    {{ $post->published_at->format('M jS Y') }}
-                </time>
-                by
-                <a class="underline" target="_blank" rel="noopener noreferrer" title="https://x.com/dasundev"
-                    href="https://x.com/dasundev">
-                    Dasun Tharanga
-                </a>
-            </p>
+            <div class="mt-2">
+                <livewire:blog.post-meta-data :$post />
+            </div>
             <p class="text-black dark:text-white mt-5 text-md">{{ $post->excerpt }}</p>
             <p class="mt-6">
                 <a class="underline text-primary-700 font-normal" href="{{ route('show-post', $post->slug) }}"
---
+++ resources/views/livewire/blog/post-meta-data.blade.php
@@ -0,0 +1,27 @@
+<div>
+    @if(! $post->isEdited())
+        <p class="text-sm text-gray-700 dark:text-gray-200">
+            Published on
+            <time datetime="{{ $post->published_at }}">
+                {{ $post->published_at->format('M jS Y') }}
+            </time>
+            by
+            <a class="underline" target="_blank" rel="noopener noreferrer" title="https://x.com/dasundev"
+               href="https://x.com/dasundev">
+                Dasun Tharanga
+            </a>
+        </p>
+    @else
+        <p class="text-sm text-gray-700 dark:text-gray-200">
+            Latest updated on
+            <time datetime="{{ $post->updated_at }}">
+                {{ $post->updated_at->format('M jS Y') }}
+            </time>
+            by
+            <a class="underline" target="_blank" rel="noopener noreferrer" title="https://x.com/dasundev"
+               href="https://x.com/dasundev">
+                Dasun Tharanga
+            </a>
+        </p>
+    @endif
+</div>
