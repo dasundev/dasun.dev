2a2de7d7e4ae6a272a337ad48413621b6b3c64f0
Dasun Tharanga <hello@dasun.dev>
2024-03-12 16:34:45 +0530

    feat: Update nova sidebar menu

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use App\Nova\Dashboards\Main;
+use App\Nova\Package;
+use App\Nova\Post;
+use App\Nova\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
 
@@ -16,6 +23,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function boot()
     {
         parent::boot();
+
+        $this->registerMenus();
     }
 
     /**
@@ -67,6 +76,32 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function tools()
     {
         return [];
+    }
+
+    /**
+     * Register all menu items.
+     *
+     * @return void
+     */
+    private function registerMenus(): void
+    {
+        Nova::mainMenu(function (Request $request) {
+            return [
+                MenuSection::dashboard(Main::class)->icon('view-grid'),
+
+                MenuSection::make('Blog', [
+                    MenuItem::resource(Post::class),
+                ])->icon('rss')->collapsable(),
+
+                MenuSection::make('Open Source', [
+                    MenuItem::resource(Package::class),
+                ])->icon('heart')->collapsable(),
+
+                MenuSection::make('Customers', [
+                    MenuItem::resource(User::class),
+                ])->icon('user')->collapsable(),
+            ];
+        });
     }
 
     /**
