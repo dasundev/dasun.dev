340d655612508bec96be77043c4f626d5adcdefb
Dasun Tharanga <hello@dasun.dev>
2024-03-12 21:51:13 +0530

    feat: Integrate laravel pulse (#28)
    * feat: Integrate laravel pulse
    
    * style: Pint fixes
    
    * test: Add pulse test
    
    * style: Pint fixes
    
    * ci: Install node modules and build assets
    
    * fix: Npm install command

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerGates();
+    }
+
+    private function registerGates(): void
+    {
+        Gate::define('viewPulse', function (User $user) {
+            return $user->isAdmin();
+        });
     }
 }
