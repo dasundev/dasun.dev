865cba8f6902280416d85b7156ae62989a5ae15f
Dasun Tharanga <hello@dasun.dev>
2024-03-20 16:25:35 +0530

    feat: Add subscribe method

--- app/Livewire/Newsletter.php
+++ app/Livewire/Newsletter.php
@@ -3,9 +3,25 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Exception;
 
 class Newsletter extends Component
 {
+    public $isSubscribed = false;
+
+    public function subscribe()
+    {
+        try {
+            // TODO
+
+            $this->isSubscribed = true;
+
+            return back()->with('success', "Success! I've just sent you an email. Simply click the link inside to confirm your subscription.");
+        } catch (Exception $e) {
+            return back()->with('error', "Apologies, but it seems we're experiencing a server error.");
+        }
+    }
+
     public function render()
     {
         return view('livewire.newsletter');
--- resources/views/livewire/newsletter.blade.php
+++ resources/views/livewire/newsletter.blade.php
@@ -1,19 +1,32 @@
 <section class="max-w-4xl mx-auto">
-    <h2 class="max-w-lg mx-auto dark:text-white text-3xl text-center font-normal">
-        Get notified for the next blog post? <span class="text-primary-700 font-semibold">Sign up</span> for the newsletter.
-    </h2>
-    <div class="mt-10 flex flex-col lg:flex-row items-center justify-center gap-2">
-        <div class="w-full lg:w-1/2">
-            <label for="email" class="sr-only">Email</label>
-            <livewire:components.forms.input
-                id="email"
-                type="email"
-                wire:model="email"
-                placeholder="Enter your email"
-            />
-        </div>
-        <div>
-            <x-button>Notify me</x-button>
-        </div>
+    @if(!$isSubscribed)
+        <h2 class="max-w-lg mx-auto dark:text-white text-3xl text-center font-normal">
+            Get notified for the next blog post? <span class="text-primary-700 font-semibold">Sign up</span> for the newsletter.
+        </h2>
+        <form wire:submit="subscribe">
+            <div class="mt-10 flex flex-col lg:flex-row items-center justify-center gap-2">
+                <div class="w-full lg:w-1/2">
+                    <label for="email" class="sr-only">Email</label>
+                    <livewire:components.forms.input
+                        id="email"
+                        type="email"
+                        wire:model="email"
+                        placeholder="Enter your email"
+                    />
+                </div>
+                <div>
+                    <x-button type="submit">Notify me</x-button>
+                </div>
+            </div>
+        </form>
+    @else
+    <div class="bg-gray-900 p-5">
+        @if(session('success'))
+            <p class="dark:text-green-500 text-start">{{ session('success') }}</p>
+        @endif
+        @if(session('error'))
+            <p class="dark:text-red-500 text-start">{{ session('error') }}</p>
+        @endif
     </div>
+    @endif
 </section>
