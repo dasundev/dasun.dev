94bcbbe14b2fe5877b407dabd60c5c6972ee40ce
Dasun Tharanga <hello@dasun.dev>
2024-03-20 15:21:05 +0530

    feat: Enhance the input component

--- resources/views/livewire/components/forms/input.blade.php
+++ resources/views/livewire/components/forms/input.blade.php
@@ -1,3 +1,1 @@
-<div>
-    <input type="{{ $type }}" id="{{ $id }}" wire:model="value" class="text-sm bg-gray-50 text-gray-900 border-0 ring-1 ring-inset p-2.5 w-full focus:ring-2 focus:ring-inset focus:ring-primary-600 dark:bg-gray-700 dark:text-white" placeholder="{{ $placeholder }}" {{ $required }} />
-</div>
+<input type="{{ $type }}" id="{{ $id }}" wire:model="value" class="w-full bg-gray-50 text-gray-900 border-0 ring-1 ring-inset p-2 focus:ring-2 focus:ring-inset focus:ring-primary-600 dark:bg-gray-700 dark:text-white" placeholder="{{ $placeholder }}" {{ $required }} />
