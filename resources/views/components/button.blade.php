b6fff5fa90ce9a0fec556bea957831a80d84f7e6
Dasun Tharanga <hello@dasun.dev>
2024-03-20 15:20:21 +0530

    feat: Update button component font weight

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -3,6 +3,6 @@
 ])
 
 <button type="{{ $type }}"
-    {{ $attributes->merge(['class' => 'font-medium text-white bg-primary-700 px-3 py-2 hover:bg-primary-800 transition-colors duration-300']) }}>
+    {{ $attributes->merge(['class' => 'font-normal text-white bg-primary-700 px-3 py-2 hover:bg-primary-800 transition-colors duration-300']) }}>
     {{ $slot }}
 </button>
