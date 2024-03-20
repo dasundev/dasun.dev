488866ac3fbdd8005ca1c2d2380ad8c8bb0c52e9
Dasun Tharanga <hello@dasun.dev>
2024-03-20 21:52:14 +0530

    fix: Remove alert message

--- resources/views/livewire/newsletter.blade.php
+++ resources/views/livewire/newsletter.blade.php
@@ -1,5 +1,4 @@
 <section class="max-w-4xl mx-auto">
-    @if(!$isSubscribed)
         <h2 class="max-w-lg mx-auto dark:text-white text-3xl text-center font-normal">
             Get notified for the next blog post? <span class="text-primary-700 font-semibold">Sign up</span> for the newsletter.
         </h2>
@@ -19,14 +18,4 @@
                 </div>
             </div>
         </form>
-    @else
-    <div class="bg-gray-900 p-5">
-        @if(session('success'))
-            <p class="dark:text-green-500 text-start">{{ session('success') }}</p>
-        @endif
-        @if(session('error'))
-            <p class="dark:text-red-500 text-start">{{ session('error') }}</p>
-        @endif
-    </div>
-    @endif
 </section>
