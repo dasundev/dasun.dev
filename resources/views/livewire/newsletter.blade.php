3f5af8f83ede0585519b56733a73c3410b2e9867
Dasun Tharanga <hello@dasun.dev>
2024-03-20 15:21:27 +0530

    fix: Improve the newsletter component

--- resources/views/livewire/newsletter.blade.php
+++ resources/views/livewire/newsletter.blade.php
@@ -1,10 +1,9 @@
-<div class="max-w-4xl mx-auto py-10 flex flex-col items-center justify-center px-10 mt-10">
-    <p class="dark:text-white text-2xl text-center font-normal">
-        Get notified for the next blog post? </br>
-        <span class="text-primary-700 font-semibold">Sign up</span> for the newsletter.
-    </p>
-    <div class="w-full flex items-center gap-2 mt-10">
-        <div class="w-full h-full">
+<section class="max-w-4xl mx-auto pb-10">
+    <h2 class="max-w-lg mx-auto dark:text-white text-3xl text-center font-normal">
+        Get notified for the next blog post? <span class="text-primary-700 font-semibold">Sign up</span> for the newsletter.
+    </h2>
+    <div class="mt-10 flex flex-col lg:flex-row items-center justify-center gap-3">
+        <div class="w-full lg:w-1/2">
             <label for="email" class="sr-only">Email</label>
             <livewire:components.forms.input
                 id="email"
@@ -13,6 +12,8 @@
                 placeholder="Enter your email"
             />
         </div>
-        <x-button class="w-1/2 h-full">Notify me</x-button>
+        <div>
+            <x-button>Notify me</x-button>
+        </div>
     </div>
-</div>
+</section>
