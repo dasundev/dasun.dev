d60eed6d42496763ce581bf06f5fbf819761c5d8
Dasun Tharanga <hello@dasun.dev>
2024-03-20 15:32:16 +0530

    feat: Fixed the gap

--- resources/views/livewire/newsletter.blade.php
+++ resources/views/livewire/newsletter.blade.php
@@ -2,7 +2,7 @@
     <h2 class="max-w-lg mx-auto dark:text-white text-3xl text-center font-normal">
         Get notified for the next blog post? <span class="text-primary-700 font-semibold">Sign up</span> for the newsletter.
     </h2>
-    <div class="mt-10 flex flex-col lg:flex-row items-center justify-center gap-3">
+    <div class="mt-10 flex flex-col lg:flex-row items-center justify-center gap-2">
         <div class="w-full lg:w-1/2">
             <label for="email" class="sr-only">Email</label>
             <livewire:components.forms.input
