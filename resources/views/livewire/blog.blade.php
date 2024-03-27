b9ad11046733be9a34e51c2ec88f630cb67c7246
Dasun Tharanga <hello@dasun.dev>
2024-03-27 22:31:21 +0530

    feat: Add ui transitions

--- resources/views/livewire/blog.blade.php
+++ resources/views/livewire/blog.blade.php
@@ -4,17 +4,14 @@
         <p class="text-center text-md text-gray-500 mt-2">{{ __('Web Development Tips, Tricks, and Insights') }}</p>
     </div>
     @foreach ($posts as $post)
-        <article class="py-10 border border-gray-100 dark:border-gray-900 px-10 mb-8">
-            <a class="text-xl lg:text-2xl font-normal text-black dark:text-white"
-                href="{{ route('show-post', $post->slug) }}" wire:navigate.hover>{{ $post->title }}</a>
-            <div class="mt-2">
-                <livewire:blog.post-meta-data :$post />
-            </div>
-            <p class="text-black dark:text-white mt-5 text-md">{{ $post->excerpt }}</p>
-            <p class="mt-6">
-                <a class="underline text-primary-700 font-normal" href="{{ route('show-post', $post->slug) }}"
-                    wire:navigate.hover>{{ __('Read more') }}</a>
-            </p>
-        </article>
+        <div class="py-10 px-5 lg:px-10 mb-8 transition transform border border-gray-100 dark:border-gray-900 hover:scale-105 hover:border-gray-200 dark:hover:border-gray-800 motion-reduce:transition-none motion-reduce:hover:transform-none">
+            <a href="{{ route('show-post', $post->slug) }}" wire:navigate.hover>
+                <div class="text-xl lg:text-2xl font-normal text-black dark:text-white">{{ $post->title }}</div>
+                <div class="mt-2">
+                    <livewire:blog.post-meta-data :$post :key="$post->id" />
+                </div>
+                <p class="text-gray-500 mt-5">{{ $post->excerpt }}</p>
+            </a>
+        </div>
     @endforeach
 </div>
