e3531f58014959ee8e272196684b9f24145dd15c
Dasun Tharanga <hello@dasun.dev>
2024-03-27 22:35:09 +0530

    feat: Enhance blog page user inteface (#43)
    * feat: Add ui transitions
    
    * feat: Update ui

--- resources/views/livewire/blog/post-meta-data.blade.php
+++ resources/views/livewire/blog/post-meta-data.blade.php
@@ -6,10 +6,14 @@
                 {{ $post->published_at->format('M jS Y') }}
             </time>
             by
-            <a class="underline" target="_blank" rel="noopener noreferrer" title="https://x.com/dasundev"
-               href="https://x.com/dasundev">
-                Dasun Tharanga
-            </a>
+            @if(Route::is('blog'))
+                <span class="underline">Dasun Tharanga</span>
+            @else
+                <a class="underline" target="_blank" rel="noopener noreferrer" title="https://x.com/dasundev"
+                   href="https://x.com/dasundev">
+                    Dasun Tharanga
+                </a>
+            @endif
         </p>
     @else
         <p class="text-sm text-gray-700 dark:text-gray-200">
@@ -18,10 +22,14 @@
                 {{ $post->updated_at->format('M jS Y') }}
             </time>
             by
-            <a class="underline" target="_blank" rel="noopener noreferrer" title="https://x.com/dasundev"
-               href="https://x.com/dasundev">
-                Dasun Tharanga
-            </a>
+            @if(Route::is('blog'))
+                <span class="underline">Dasun Tharanga</span>
+            @else
+                <a class="underline" target="_blank" rel="noopener noreferrer" title="https://x.com/dasundev"
+                   href="https://x.com/dasundev">
+                    Dasun Tharanga
+                </a>
+            @endif
         </p>
     @endif
 </div>
