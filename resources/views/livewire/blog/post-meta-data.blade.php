11f493076b98192e0d5e53280cec9817d129349c
Dasun Tharanga <hello@dasun.dev>
2024-03-28 12:08:15 +0530

    feat: Add read time to blog posts (#44)
    * feat: Add read time to blog posts
    
    * style: Pint fixes

--- resources/views/livewire/blog/post-meta-data.blade.php
+++ resources/views/livewire/blog/post-meta-data.blade.php
@@ -1,35 +1,22 @@
-<div>
-    @if(! $post->isEdited())
-        <p class="text-sm text-gray-700 dark:text-gray-200">
+<div class="flex flex-col sm:flex-row items-start sm:items-center gap-1 sm:gap-2">
+    <p class="text-sm text-gray-700 dark:text-gray-200">
+        @if(! $post->isEdited())
             Published on
             <time datetime="{{ $post->published_at }}">
                 {{ $post->published_at->format('M jS Y') }}
             </time>
-            by
-            @if(Route::is('blog'))
-                <span class="underline">Dasun Tharanga</span>
-            @else
-                <a class="underline" target="_blank" rel="noopener noreferrer" title="https://x.com/dasundev"
-                   href="https://x.com/dasundev">
-                    Dasun Tharanga
-                </a>
-            @endif
-        </p>
-    @else
-        <p class="text-sm text-gray-700 dark:text-gray-200">
+        @else
             Last updated on
             <time datetime="{{ $post->updated_at }}">
                 {{ $post->updated_at->format('M jS Y') }}
             </time>
-            by
-            @if(Route::is('blog'))
-                <span class="underline">Dasun Tharanga</span>
-            @else
-                <a class="underline" target="_blank" rel="noopener noreferrer" title="https://x.com/dasundev"
-                   href="https://x.com/dasundev">
-                    Dasun Tharanga
-                </a>
-            @endif
-        </p>
-    @endif
+        @endif
+        by
+        <a class="underline" target="_blank" rel="noopener noreferrer" title="https://x.com/dasundev"
+           href="https://x.com/dasundev">
+            Dasun Tharanga
+        </a>
+    </p>
+    <span class="text-gray-700 dark:text-gray-200 hidden sm:block">·</span>
+    <span class="text-sm text-gray-700 dark:text-gray-200">{{ Str::readTime($post->content) }} min read</span>
 </div>
