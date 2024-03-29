8c7249d8fb534825e14130cb0035aa13eb6bc7ca
Dasun Tharanga <hello@dasun.dev>
2024-03-29 07:33:01 +0530

    feat: Install larabug

--- config/logging.php
+++ config/logging.php
@@ -54,7 +54,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'larabug'],
             'ignore_exceptions' => false,
         ],
 
@@ -125,6 +125,10 @@ return [
 
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
+        ],
+
+        'larabug' => [
+            'driver' => 'larabug',
         ],
     ],
 
