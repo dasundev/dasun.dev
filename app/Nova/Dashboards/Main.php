f9828a62064098fec9f230b299efac1f524b1b36
Dasun Tharanga <hello@dasun.dev>
2024-03-31 16:42:04 +0530

    feat(nova): Add newsletter subscribers value to main dashboard

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -2,20 +2,18 @@
 
 namespace App\Nova\Dashboards;
 
-use Laravel\Nova\Cards\Help;
+use App\Nova\Metrics\NewsletterSubscribers;
 use Laravel\Nova\Dashboards\Main as Dashboard;
 
 class Main extends Dashboard
 {
     /**
      * Get the cards for the dashboard.
-     *
-     * @return array
      */
-    public function cards()
+    public function cards(): array
     {
         return [
-            new Help,
+            new NewsletterSubscribers,
         ];
     }
 }
