7ce15a2f77e1bdef22665908f5625082226bec26
Dasun Tharanga <hello@dasun.dev>
2024-03-31 17:24:12 +0530

    test: Add newsletter test cases

---
+++ tests/Feature/Livewire/NewsletterTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature\Livewire;
+
+use App\Livewire\About;
+use App\Livewire\Newsletter;
+use App\Models\NewsletterSubscriber;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class NewsletterTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function renders_successfully()
+    {
+        Livewire::test(Newsletter::class)
+            ->assertStatus(200)
+            ->assertSee('newsletter');
+    }
+
+    /** @test */
+    public function can_set_email()
+    {
+        Livewire::test(Newsletter::class)
+            ->set('email', 'hello@dasun.dev')
+            ->assertSet('email', 'hello@dasun.dev')
+            ->assertHasNoErrors();
+    }
+
+    /** @test */
+    public function email_field_is_required()
+    {
+        Livewire::test(Newsletter::class)
+            ->set('email', '')
+            ->call('subscribe')
+            ->assertHasErrors(['email' => 'required']);
+    }
+
+    /** @test */
+    public function email_field_has_a_valid_email()
+    {
+        Livewire::test(Newsletter::class)
+            ->set('email', 'invalid-email')
+            ->call('subscribe')
+            ->assertHasErrors(['email' => 'email']);
+    }
+
+    /** @test */
+    public function user_can_subscribe()
+    {
+        Livewire::test(Newsletter::class)
+            ->set('email', 'hello@dasun.dev')
+            ->call('subscribe')
+            ->assertDispatched('subscribed')
+            ->assertHasNoErrors();
+    }
+
+    /** @test */
+    public function user_cannot_subscribe_more_than_onetime()
+    {
+        for ($x = 0; $x <= 3; $x++) {
+            Livewire::test(Newsletter::class)
+                ->set('email', 'hello@dasun.dev')
+                ->call('subscribe')
+                ->assertDispatched('subscribed')
+                ->assertHasNoErrors();
+        }
+
+        $this->assertCount(1, NewsletterSubscriber::all());
+    }
+}
