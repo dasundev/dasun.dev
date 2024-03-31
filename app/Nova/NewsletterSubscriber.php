7bbe91df54b6ec9fa5ac9a88d00eb49986fefd02
Dasun Tharanga <hello@dasun.dev>
2024-03-31 16:11:16 +0530

    feat(nova): Create newsletter

---
+++ app/Nova/NewsletterSubscriber.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Date;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class NewsletterSubscriber extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\NewsletterSubscriber>
+     */
+    public static string $model = \App\Models\NewsletterSubscriber::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+        'email',
+    ];
+
+    public static function label(): string
+    {
+        return 'Subscribers';
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     */
+    public function fields(NovaRequest $request): array
+    {
+        return [
+            ID::make()->sortable(),
+            Text::make('email'),
+            Date::make('email_verified_at'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     */
+    public function cards(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     */
+    public function filters(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     */
+    public function lenses(NovaRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     */
+    public function actions(NovaRequest $request): array
+    {
+        return [];
+    }
+}
