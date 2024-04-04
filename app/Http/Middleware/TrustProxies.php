f143cc05e86029bf982e29c10cc11c9360883cd6
Dasun Tharanga <hello@dasun.dev>
2024-04-04 10:08:00 +0530

    feat: remove trust proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,9 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = [
-        '127.0.0.1',
-    ];
+    protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
