4b7298e5f425359bbbbcf15c179d4adc16d3c747
Dasun Tharanga <hello@dasun.dev>
2024-05-01 18:36:02 +0530

    feat: import the static assets into the application's entry point

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@ import "./docsearch";
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import clipboard from "./clipboard.js";
 
+import.meta.glob([
+    '../images/**',
+]);
+
 Alpine.data('clipboard', clipboard)
 
 Livewire.start()
