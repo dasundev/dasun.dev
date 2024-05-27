8aa2a1fae3ae8f2bf69f0624d8e9cbecc726f868
Dasun Tharanga <hello@dasun.dev>
2024-05-27 07:23:21 +0530

    fix: update `price` column

--- database/migrations/2024_05_26_163142_add_price_to_packages_table.php
+++ database/migrations/2024_05_26_163142_add_price_to_packages_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('packages', function (Blueprint $table) {
-            $table->integer('price')->nullable();
+            $table->unsignedInteger('price')->nullable();
         });
     }
 
