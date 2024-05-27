f9c02d6739365d7d1209910bf8a45e68290b624b
Dasun Tharanga <hello@dasun.dev>
2024-05-27 07:21:07 +0530

    feat: update order line model

--- app/Models/OrderLine.php
+++ app/Models/OrderLine.php
@@ -5,10 +5,18 @@ namespace App\Models;
 use Dasundev\PayHere\Models\Contracts\PayHereOrderLine;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class OrderLine extends Model implements PayHereOrderLine
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function purchasable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 
     public function payHereOrderLineId(): string
     {
