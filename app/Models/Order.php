d8a635472e0aaacec84c9082e72bed99430a4d47
Dasun Tharanga <hello@dasun.dev>
2024-05-27 07:21:22 +0530

    feat: update order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,14 +5,17 @@ namespace App\Models;
 use Dasundev\PayHere\Models\Contracts\PayHereOrder;
 use Dasundev\PayHere\Models\Payment;
 use Dasundev\PayHere\Models\Subscription;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model implements PayHereOrder
 {
     use HasFactory;
+    use HasUuids;
 
     protected $guarded = [];
 
@@ -29,5 +32,10 @@ class Order extends Model implements PayHereOrder
     public function lines(): HasMany
     {
         return $this->hasMany(OrderLine::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
