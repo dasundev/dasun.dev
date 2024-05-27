08df3432491172992bc1bc4ec0f7c984af59bdd6
Dasun Tharanga <hello@dasun.dev>
2024-05-27 07:49:49 +0530

    feat: add `slug` column after `name` column

--- database/migrations/2024_05_27_021232_add_slug_to_packages_table.php
+++ database/migrations/2024_05_27_021232_add_slug_to_packages_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('packages', function (Blueprint $table) {
-            $table->string('slug')->unique();
+            $table->string('slug')->after('name')->unique();
         });
     }
 
