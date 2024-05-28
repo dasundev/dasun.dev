60fc95f790234ff8e11d0ef868381771b2b7566a
Dasun Tharanga <hello@dasun.dev>
2024-05-28 10:35:46 +0530

    feat: update navigation

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -30,15 +30,7 @@ $logout = function (Logout $logout) {
                     <div class="hidden sm:flex sm:items-center">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
-                                <button class="inline-flex items-center px-3 py-[10px] border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                                    <div x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
-
-                                    <div class="ms-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
+                                <img class="rounded-full ring-2 ring-primary-700 cursor-pointer" width="36" height="36" src="{{ Gravatar::get(auth()->user()->email) }}" alt="{{ auth()->user()->name }}">
                             </x-slot>
                             <x-slot name="content">
                                 <x-dropdown-link :href="route('profile')" wire:navigate>
