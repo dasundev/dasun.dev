3566afc8215475a08c066115593f9f34f07fc010
Dasun Tharanga <hello@dasun.dev>
2024-05-29 12:02:38 +0530

    feat: update forgot password page

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -37,7 +37,9 @@ $sendPasswordResetLink = function () {
 ?>
 
 <div>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+    <h2 class="text-lg leading-6 font-medium dark:text-white">Reset Password</h2>
+
+    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400 mt-5">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
