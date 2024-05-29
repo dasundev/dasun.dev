048e489a6a056c848ff634a984b5df5868981a91
Dasun Tharanga <hello@dasun.dev>
2024-05-29 12:09:00 +0530

    feat: implements `MustVerifyEmail` contract

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Dasundev\PayHere\Billable;
 use Dasundev\PayHere\Models\Contracts\PayHereCustomer;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -12,7 +13,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements PayHereCustomer
+class User extends Authenticatable implements PayHereCustomer, MustVerifyEmail
 {
     use Billable, HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
