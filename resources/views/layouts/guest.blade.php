e713bbfe4bde7b1dae0691bc352637e0e34921a6
Dasun Tharanga <hello@dasun.dev>
2024-05-29 11:47:22 +0530

    feat: update guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,20 +11,46 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Fonts -->
+        @googlefonts
+        @googlefonts('overpass')
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
-
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-black">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-black shadow-md overflow-hidden border border-gray-900 rounded-md">
                 {{ $slot }}
             </div>
         </div>
+
+        <script>
+            if (localStorage.getItem('darkMode_on') === 'true' || (!('darkMode_on' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.querySelector('html').classList.add('dark');
+            } else {
+                document.querySelector('html').classList.remove('dark');
+            }
+
+            document.addEventListener('alpine:init', () => {
+                Alpine.store('darkMode', {
+                    on: Alpine.$persist(true).as('darkMode_on'),
+
+                    toggle() {
+                        this.on = !this.on
+                    },
+                })
+
+                Alpine.effect(() => {
+                    const darkMode = Alpine.store('darkMode').on;
+
+                    if (darkMode) {
+                        document.documentElement.classList.add('dark')
+                    } else {
+                        document.documentElement.classList.remove('dark')
+                    }
+                })
+            })
+        </script>
     </body>
 </html>
