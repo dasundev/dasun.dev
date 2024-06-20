22ffed52658879573316fe008c37a272c008eef2
Dasun Tharanga <hello@dasun.dev>
2024-06-20 21:35:12 +0530

    style: pint fixes

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -7,7 +7,6 @@ use Laravel\Nova\Fields\Gravatar;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Password;
 use Laravel\Nova\Fields\Text;
-use Laravel\Nova\Fields\Textarea;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 class User extends Resource
