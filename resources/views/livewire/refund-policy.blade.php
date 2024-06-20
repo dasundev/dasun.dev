97ce5ac9a2a1d001a56c412c6be4da4449fd4bbb
Dasun Tharanga <hello@dasun.dev>
2024-06-20 12:52:09 +0530

    feat: add refund policy

---
+++ resources/views/livewire/refund-policy.blade.php
@@ -0,0 +1,26 @@
+<div class="max-w-3xl mx-auto my-10 px-5 lg:px-0">
+    <div class="mb-10">
+        <h2 class="text-3xl lg:text-4xl text-center text-black dark:text-white">{{ __('Refund Policy') }}</h2>
+        <p class="text-black dark:text-white mt-5">
+            Thank you for purchasing Laravel plugins from us. We appreciate your business and strive to provide high-quality digital products. However, please note that all sales are final. We do not offer refunds or exchanges for any digital products purchased through our website.
+        </p>
+        <h3 class="text-xl text-black dark:text-white mt-5">No Refunds</h3>
+        <p class="text-black dark:text-white my-5">Due to the nature of digital products and the fact that they cannot be "returned," we do not offer refunds or exchanges for any reason.</p>
+        <h3 class="text-xl text-black dark:text-white mt-5">Exceptions</h3>
+        <p class="text-black dark:text-white my-5">
+            In rare cases, exceptions may be made at our sole discretion under the following circumstances:
+        </p>
+        <ul>
+            <li class="text-black dark:text-white">1. If the digital product was not delivered as described.</li>
+            <li class="text-black dark:text-white">2. If the product is proven to be defective and cannot be fixed within a reasonable timeframe.</li>
+        </ul>
+        <h3 class="text-xl text-black dark:text-white mt-5">How to Request an Exception</h3>
+        <p class="text-black dark:text-white my-5">
+            If you believe you qualify for an exception, please contact us within 14 days of purchase. Provide your order number, a detailed description of the issue, and any relevant information that can help us evaluate your claim.
+        </p>
+        <h3 class="text-xl text-black dark:text-white mt-5">Contact Us</h3>
+        <p class="text-black dark:text-white leading-7">
+            If you have any questions regarding our Refund Policy or wish to request an exception, please reach out to Dasun Tharanga at <a href="mailto:hello@dasun.dev" class="text-primary-700 font-medium underline">hello@dasun.dev</a>.
+        </p>
+    </div>
+</div>
