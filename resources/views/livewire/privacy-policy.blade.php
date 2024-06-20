a4321a80da7f46aed5a4c04e3bdd2d32520cff88
Dasun Tharanga <hello@dasun.dev>
2024-06-20 13:23:19 +0530

    feat: add privacy policy page

---
+++ resources/views/livewire/privacy-policy.blade.php
@@ -0,0 +1,50 @@
+<div class="max-w-3xl mx-auto my-10 px-5 lg:px-0">
+    <div class="mb-10">
+        <h2 class="text-3xl lg:text-4xl text-center text-black dark:text-white">{{ __('Privacy Policy') }}</h2>
+        <p class="text-black dark:text-white mt-5">
+            I, Dasun Tharanga, operate this website selling Laravel plugins. I am committed to protecting your privacy. This Privacy Policy explains how I collect, use, disclose, and safeguard your information when you visit my website and provide me with your personal data.
+        </p>
+        <h3 class="text-xl text-black dark:text-white mt-5">Information I Collect</h3>
+        <p class="text-black dark:text-white my-5">
+            I collect and process the following personal data about you:
+        </p>
+        <ul>
+            <li class="text-black dark:text-white">1. Name - Your full name.</li>
+            <li class="text-black dark:text-white">2. Email - Your email address for communication purposes.</li>
+            <li class="text-black dark:text-white">3. Password - Encrypted password for accessing your account.</li>
+        </ul>
+        <h3 class="text-xl text-black dark:text-white mt-5">Use of Information</h3>
+        <p class="text-black dark:text-white my-5">
+            I use the information I collect in the following ways:
+        </p>
+        <ul>
+            <li class="text-black dark:text-white">1. To create and manage your account for purchasing Laravel plugins.</li>
+            <li class="text-black dark:text-white">2. To communicate with you regarding your orders, account updates, and customer support inquiries.</li>
+        </ul>
+        <h3 class="text-xl text-black dark:text-white mt-5">Data Security</h3>
+        <p class="text-black dark:text-white leading-7">
+            I implement appropriate technical and organizational measures to protect your personal data against unauthorized access, alteration, disclosure, or destruction.
+        </p>
+        <h3 class="text-xl text-black dark:text-white mt-5">Information Sharing</h3>
+        <p class="text-black dark:text-white leading-7 my-5">
+            We respect your privacy and do not sell, trade, or otherwise transfer your personal information to third parties without your consent, except in the following circumstances:
+        </p>
+        <ul>
+            <li class="text-black dark:text-white">1. To create and manage your account for purchasing Laravel plugins.</li>
+            <li class="text-black dark:text-white">2. To communicate with you regarding your orders, account updates, and customer support inquiries.</li>
+            <li class="text-black dark:text-white">3. PayHere payment processing: We may share your information with PayHere, our trusted third-party payment processor, solely for the purpose of processing payments you make on our website. PayHere is contractually obligated to handle your data securely and confidentially.</li>
+        </ul>
+        <h3 class="text-xl text-black dark:text-white mt-5">Cookies and Tracking Technologies</h3>
+        <p class="text-black dark:text-white leading-7">
+            We use cookies and similar technologies to enhance your browsing experience, analyze website traffic, and gather information about your preferences and interactions with our website. You have the option to disable cookies through your browser settings, but this may limit certain features and functionality of our website.
+        </p>
+        <h3 class="text-xl text-black dark:text-white mt-5">Cookies and Tracking Technologies</h3>
+        <p class="text-black dark:text-white leading-7">
+            We use cookies and similar technologies to enhance your browsing experience, analyze website traffic, and gather information about your preferences and interactions with our website. You have the option to disable cookies through your browser settings, but this may limit certain features and functionality of our website.
+        </p>
+        <h3 class="text-xl text-black dark:text-white mt-5">Contact Us</h3>
+        <p class="text-black dark:text-white leading-7">
+            If you have any questions, concerns, or requests regarding our Privacy Policy or the handling of your personal information, please reach out to Dasun Tharanga at <a href="mailto:hello@dasun.dev" class="text-primary-700 font-medium underline">hello@dasun.dev</a>.
+        </p>
+    </div>
+</div>
