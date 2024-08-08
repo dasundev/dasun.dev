0612eeecda2aea1fdb60fae7a0c529e960f3a399
Dasun Tharanga <hello@dasun.dev>
2024-08-08 22:08:16 +0530

    wip

---
+++ app/Http/Controllers/Api/SatisAuthenticationController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\License;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Http\Request;
+
+class SatisAuthenticationController extends Controller
+{
+    public function __invoke(Authenticatable $license, Request $request)
+    {
+        /** @var $license License */
+        if (! $license instanceof License) {
+            abort(401);
+        }
+
+        $package = $this->getRequestedPackage($request);
+
+        $hasAccess = License::query()
+            ->with(['purchasable'])
+            ->whereNotExpired()
+            ->where('user_id', $license->user_id)
+            ->get()
+            ->contains(
+                fn (License $license) => $license->purchasable->includesPackageAccess($package)
+            );
+
+        abort_unless($hasAccess, 401);
+
+        return response('valid');
+    }
+
+    protected function getRequestedPackage(Request $request): string
+    {
+        $originalUrl = $request->header('X-Original-URI', '');
+
+        preg_match('#^/dist/(?<package>dasundev/[^/]*)/#', $originalUrl, $matches);
+
+        if (! key_exists('package', $matches)) {
+            abort(401, 'Missing X-Original-URI header');
+        }
+
+        return $matches['package'];
+    }
+}
