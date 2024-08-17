95859cd1c0f00d51873589bca729e95b56c2212a
Dasun Tharanga <hello@dasun.dev>
2024-08-17 19:44:55 +0530

    wip

--- app/Console/Commands/UpdatePackageTags.php
+++ app/Console/Commands/UpdatePackageTags.php
@@ -2,7 +2,15 @@
 
 namespace App\Console\Commands;
 
+use App\Http\Integrations\GitHub\GitHubConnector;
+use App\Http\Integrations\GitHub\Requests\ListRepositoryTags;
+use App\Http\Integrations\Packagist\PackagistConnector;
+use App\Http\Integrations\Packagist\Requests\GetPackageDataRequest;
+use App\Models\Package;
+use App\Repositories\Contracts\PackageRepository;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UpdatePackageTags extends Command
 {
@@ -10,5 +18,21 @@ class UpdatePackageTags extends Command
 
     protected $description = 'Fetch and update all tags for packages from GitHub.';
 
-    public function handle(): void {}
+    public function handle(): void
+    {
+        Package::all()->each(function ($package) {
+            $connector = new GitHubConnector;
+
+            $response = $connector->send(new ListRepositoryTags($package->composer_package));
+
+            $tags = $response->collect()->map(function ($tag) {
+                return [
+                    'name' => $tag['name'],
+                    'sha' => $tag['commit']['sha'],
+                ];
+            });
+
+            $package->update(['tags' => $tags]);
+        });
+    }
 }
