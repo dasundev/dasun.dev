7862ab6813c8d764dbeb1014544a5c92b8c3516d
Dasun Tharanga <hello@dasun.dev>
2024-08-17 19:05:25 +0530

    wip

---
+++ database/migrations/2024_08_17_133509_add_fallback_version_to_licenses_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('licenses', function (Blueprint $table) {
+            $table->json('fallback_version')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('licenses', function (Blueprint $table) {
+            $table->dropColumn('fallback_version');
+        });
+    }
+};
