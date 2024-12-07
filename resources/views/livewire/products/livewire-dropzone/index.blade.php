582381afe5a95fc0d70ad4ce7ac952bf280495e6
Dasun Tharanga <hello@dasun.dev>
2024-12-07 22:31:38 +0530

    wip

--- resources/views/livewire/products/livewire-dropzone/index.blade.php
+++ resources/views/livewire/products/livewire-dropzone/index.blade.php
@@ -1,5 +1,6 @@
 <div class="bg-primary-700 py-32">
     <div class="max-w-7xl mx-auto">
         <h1 class="text-6xl text-white font-black leading-tight">Premium Themes <br>for <a href="#" class="underline underline-offset-[6%] decoration-[8%] decoration-white/20 hover:decoration-white transition-colors">Livewire Dropzone</a></h1>
+        <p class="text-white text-lg mt-8 leading-normal">The default theme works well, but you can enhance the Livewire Dropzone <br> further with our premium themes.</p>
     </div>
 </div>
