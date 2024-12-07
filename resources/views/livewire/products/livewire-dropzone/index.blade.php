0be12eb308f1b98a1ce429bba0850b898de5c1a6
Dasun Tharanga <hello@dasun.dev>
2024-12-07 22:27:55 +0530

    wip

--- resources/views/livewire/products/livewire-dropzone/index.blade.php
+++ resources/views/livewire/products/livewire-dropzone/index.blade.php
@@ -1,1 +1,5 @@
-<div></div>
+<div class="bg-primary-700 py-32">
+    <div class="max-w-7xl mx-auto">
+        <h1 class="text-7xl text-white font-black leading-tight">Premium Themes <br>for <a href="#" class="underline underline-offset-[6%] decoration-[8%] decoration-white/20 hover:decoration-white transition-colors">Livewire Dropzone</a></h1>
+    </div>
+</div>
