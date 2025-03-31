6c40914f5faeaa7c1c93da92ca120f8d75d7bbf1
Dasun Tharanga <hello@dasun.dev>
2025-03-31 08:26:03 +0530

    test: add unit test

---
+++ tests/Unit/Models/PostTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use App\Models\Post;
+
+test('to array', function () {
+    $package = Post::factory()->create()->fresh();
+
+    expect(array_keys($package->toArray()))->toBe([
+        'id',
+        'title',
+        'slug',
+        'excerpt',
+        'content',
+        'published_at',
+        'created_at',
+        'updated_at',
+    ]);
+});
