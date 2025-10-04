2762d3a6ef97ee10b46b383d9e0fad5b425d9c01
Dasun Tharanga <hello@dasun.dev>
2025-10-04 16:23:35 +0530

    fix: use `@verbatim`

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -21,7 +21,9 @@
     <meta name="twitter:description" content="{{ $description }}" />
     <meta name="twitter:image" content="{{ asset('assets/social.png') }}" />
 
-    <script type="application/ld+json">{"@context":"https://schema.org","@type":"WebPage","url":"{{ url()->current() }}"}</script>
+    @verbatim
+        <script type="application/ld+json">{"@context":"https://schema.org","@type":"WebPage","url":"{{ url()->current() }}"}</script>
+    @endverbatim
 
     <title>{{ $title }}</title>
 
